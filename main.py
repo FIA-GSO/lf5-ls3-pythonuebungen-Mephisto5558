ad248b1dbd69d04c40479b5e04b2f94336b04c73
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-03-08 12:38:49 +0100

    formatting

--- main.py
+++ main.py
@@ -2,21 +2,21 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
 
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#---------------------Aufgabe 1 ------------------------------------
-def compute_r2d2_population(steps: int) -> tuple[int,int,int]:
+# ---------------------Aufgabe 1 ------------------------------------
+def compute_r2d2_population(steps: int) -> tuple[int, int, int]:
     """
         Computes the r2d2 population for the given step amount
     :param steps: amount of steps to compute the population (e.g.: 5)
     :return: tuple of childs adults and old r2d2
     """
-    return (0,0,0)
+    return (0, 0, 0)
 
-#---------------------Aufgabe 2 Streichholz------------------------------
-#IMPLEMENT YOUR SOLUTION FOR THE STEICHHOLZSPIEL HERE
+# ---------------------Aufgabe 2 Streichholz------------------------------
+# IMPLEMENT YOUR SOLUTION FOR THE STEICHHOLZSPIEL HERE
 
 
-#---------------------Aufgabe 3 Heron ------------------------------------
-def heron_verfahren(area : float, threshold:float) -> float:
+# ---------------------Aufgabe 3 Heron ------------------------------------
+def heron_verfahren(area: float, threshold: float) -> float:
     """
         computes the square root using the heron method
     :param area: size of the area e.g.25
@@ -27,19 +27,19 @@ def heron_verfahren(area : float, threshold:float) -> float:
     return 0
 
 
-#---------------------Aufgabe 4 Quersumme------------------------------
-#IMPLEMENT, IF NECESSARY, EXERCISE 4 HERE BUT USE A FUNCTION!
+# ---------------------Aufgabe 4 Quersumme------------------------------
+# IMPLEMENT, IF NECESSARY, EXERCISE 4 HERE BUT USE A FUNCTION!
 
 
-#---------------MANAGEMENT----------------------
-#-------------COMMENT/UNCOMMENT lines to launch the different exercises
+# ---------------MANAGEMENT----------------------
+# -------------COMMENT/UNCOMMENT lines to launch the different exercises
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("You need to adjust this code to run your implementation")
 
     # Aufgabe 1
     print(f"""
-        # R2D2 Population after 5 steps is: 
+        # R2D2 Population after 5 steps is:
         # Young: {compute_r2d2_population(5)[0]}
         # Adults: {compute_r2d2_population(5)[1]}
         # Old: {compute_r2d2_population(5)[2]}""")
@@ -49,7 +49,8 @@ if __name__ == '__main__':
     # TO BE IMPLEMENTED
 
     # Aufgabe 3
-    print (f"Die Wurzel für die Fläche 25 und Grenze 0.01 nach Heron ist: {heron_verfahren(25, 0.01)}")
+    print(f"Die Wurzel für die Fläche 25 und Grenze 0.01 nach Heron ist: {
+          heron_verfahren(25, 0.01)}")
 
     # Aufgabe 4
     # TO BE IMPLEMENTED
